b5672cf0e3e2a0f697a5fed00cf4341717089e53
Valeria Arenas <110551979+Varenas21@users.noreply.github.com>
2025-06-27 17:30:09 -0600

    Time & Date are fixed. Added navbar options, and changed color

--- script.js
+++ script.js
@@ -15,10 +15,11 @@ let years = TIMER.getFullYear();
     if (hours === 0){ hours = 12; } // Change to be not 24 hour clock
     if (hours > 12){hours = hours - 12; timeSession="PM"} //Subtract from 12 hours EX: 13 - 12 = 1 PM
 
-    
+    hours = (hours < 10) ? "0" + hours : hours; //If the hours are less than 10 then add a 0 (01: 02 PM)
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
 
     const timeString = `${hours}:${minutes} ${timeSession} ${days}/${months}/${years}`;
     document.getElementById('TimeDate').innerHTML = timeString;
 }
 SetClock();
-setInterval(SetClock, 1000);
+setInterval(SetClock, 1000); // Change every 10000 seconds
