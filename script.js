e12d5914db8d9935a13a0de79f1dbbc68003cf19
Valeria Arenas <110551979+Varenas21@users.noreply.github.com>
2025-06-27 17:07:13 -0600

    Time & Date included, including navbar

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // TIMER
+const TIMER = new Date();
+const DATE = new Date();
 
-let TIMER = new Date();
-let DATE = new Date();
 
-let hour = TIMER.getHours();
-let minute = TIMER.getMinutes();
+function SetClock()
+{
+let hours = TIMER.getHours();
+let minutes = TIMER.getMinutes();
+let days = TIMER.getDay();
+let months = TIMER.getMonth();
+let years = TIMER.getFullYear();
 
+    let timeSession = "AM";
+    if (hours === 0){ hours = 12; } // Change to be not 24 hour clock
+    if (hours > 12){hours = hours - 12; timeSession="PM"} //Subtract from 12 hours EX: 13 - 12 = 1 PM
+
+    
+
+    const timeString = `${hours}:${minutes} ${timeSession} ${days}/${months}/${years}`;
+    document.getElementById('TimeDate').innerHTML = timeString;
+}
+SetClock();
+setInterval(SetClock, 1000);
