0f503cb53449505e16657f5c952410873286414e
Valeria Arenas <110551979+Varenas21@users.noreply.github.com>
2025-06-30 09:56:23 -0600

    Began Timer - Changed Styling for the time and navbar - Populated Timer.HTML with description and display of date - Fixed time and date logic so that months display correctly and time updates without refreshing the website

---
+++ Scripts/Timer.js
@@ -0,0 +1,29 @@
+/* 2025 VALERIA ARENAS - BASICS IN JAVASCRIPT */
+// TIMER
+
+// Change every 10000 seconds
+function SetClock() {
+    const TIMER = new Date();
+
+    let hours = TIMER.getHours();
+    let minutes = TIMER.getMinutes();
+    let seconds = TIMER.getSeconds();
+    let days = TIMER.getDate();
+    let months = TIMER.getMonth() + 1; // January is 0, need to add 1 to get current month
+    let years = TIMER.getFullYear();
+
+    let timeSession = "AM";
+    if (hours === 0) { hours = 12; } // Change to be not 24 hour clock
+    if (hours > 12) { hours -= 12; timeSession = "PM" } //Subtract from 12 hours EX: 13 - 12 = 1 PM
+
+    hours = (hours < 10) ? "0" + hours : hours; //If the hours are less than 10 then add a 0 (01: 02 PM)
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    const TIME_STRING = `${hours}::${minutes}::${seconds} ${timeSession} ${days}/${months}/${years}`;
+    document.getElementById('TimeDate').innerHTML = TIME_STRING;
+}
+SetClock();
+setInterval(SetClock, 1000);
+
+// TIMER
--- Scripts/ToDoScript.js
+++ Scripts/ToDoScript.js
@@ -1,28 +1,5 @@
 //2025 VALERIA ARENAS - BASICS IN JAVASCRIPT
 
-// function SetClock() {
-//     // TIMER
-//     const TIMER = new Date();
-//     const DATE = new Date();
-//     let hours = TIMER.getHours();
-//     let minutes = TIMER.getMinutes();
-//     let days = TIMER.getDay();
-//     let months = TIMER.getMonth();
-//     let years = TIMER.getFullYear();
-
-//     let timeSession = "AM";
-//     if (hours === 0) { hours = 12; } // Change to be not 24 hour clock
-//     if (hours > 12) { hours = hours - 12; timeSession = "PM" } //Subtract from 12 hours EX: 13 - 12 = 1 PM
-
-//     hours = (hours < 10) ? "0" + hours : hours; //If the hours are less than 10 then add a 0 (01: 02 PM)
-//     minutes = (minutes < 10) ? "0" + minutes : minutes;
-
-//     const timeString = `${hours}:${minutes} ${timeSession} ${days}/${months}/${years}`;
-//     document.getElementById('TimeDate').innerHTML = timeString;
-// }
-// SetClock();
-// setInterval(SetClock, 1000); // Change every 10000 seconds
-
 // TO-DO LIST
 // Creating a new list, when the button is clicked, it will create a new object and add to that list.
 // Will create a nested object list, it will get the name AND will iterate through the list of items that are nested
